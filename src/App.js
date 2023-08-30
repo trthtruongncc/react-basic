bcac70bc4952c55ff121ed3dd2592d4296cf610c
unknown <trthtruongnc@yahoo.com>
2023-08-30 17:21:42 +0700

    hello lan 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello world
+          Hello world lan 2
         </a>
       </header>
     </div>
